b6de63334b96b8b6cb0ebae9aa7109343f8338cd
basilgood <elsile69@yahoo.com>
2025-07-18 04:09:42 +0300

    fix(cfg-eslint): bump cfg 1.53 to 2.3.0

---
+++ eslint.config.js
@@ -0,0 +1,16 @@
+import cfg from '@neovici/cfg/eslint/index.mjs';
+
+export default [
+	...cfg,
+	{
+		ignores: ['coverage/', 'dist/', '__snapshots__', 'storybook-static/'],
+	},
+	{
+		rules: {
+			'max-lines-per-function': 0,
+			'import/group-exports': 0,
+			'mocha/no-global-tests': 'off',
+			'mocha/max-top-level-suites': 'off',
+		},
+	},
+];
